bace25b2acf37e6e11009231960956eeea0ed1a0
walt22r <walter22r@icloud.com>
2020-06-09 13:29:09 -0500

    Delete randomize eigenvectors all vs all. Just column by column required

--- FitModel.r
+++ FitModel.r
@@ -5,7 +5,7 @@ library(EpiEstim)
 library(incidence)
 library(zoo) ##importar la librería, por favor ;)
 
-#path = "D:\\Documentos\\MT\\Mobility\\MobilityGitHub\\Data\\"
+path = "D:\\Documentos\\MT\\Mobility\\MobilityGitHub\\Data\\"
 path = "Data/"
 set.seed(1)
 mob <- read.csv(paste0(path, "GlobalMobilityReport.csv"))
@@ -16,11 +16,11 @@ estados <- c("Estados Unidos Mexicanos", "Aguascalientes", "Baja California", "B
                                               "Michoacan", "Morelos", "Nayarit", "Nuevo Leon", "Oaxaca", "Puebla", "Queretaro", "Quintana Roo", "San Luis Potosi",
                                               "Sinaloa", "Sonora", "Mexico", "Tabasco", "Tamaulipas", "Tlaxcala", "Veracruz", "Yucatan", "Zacatecas")
 mob$sub_region_1 <- as.character(mob$sub_region_1)
-#mob$sub_region_1 <- enc2utf8(mob$sub_region_1)
+mob$sub_region_1 <- enc2utf8(mob$sub_region_1)
 mob$date <- as.Date(mob$date, format="%Y-%m-%d")
 
 for (state in seq(length(states))){
-    mob$sub_region_1[mob$sub_region_1 == states[state]] <- estados[state]
+  mob$sub_region_1[mob$sub_region_1 == states[state]] <- estados[state]
 }
 
 
@@ -40,19 +40,21 @@ file.entidades <- data.frame(id = c(seq(1, 32), 36, 97, 98, 99), entidad = c("Ag
 file.entidades$entidad <- as.character(file.entidades$entidad)
 #file.entidades$entidad <- enc2utf8(file.entidades$entidad)
 totdata <- read.csv(paste0(path, "TotalMX.csv"))
-
-totdata$FECHA_ACTUALIZACION = as.Date(totdata$FECHA_ACTUALIZACION)
-totdata$FECHA_INGRESO = as.Date(totdata$FECHA_INGRESO)
-totdata$FECHA_SINTOMAS = as.Date(totdata$FECHA_SINTOMAS)
+totdata$FECHA_ACTUALIZACION = as.Date(totdata$FECHA_ACTUALIZACION, format="%Y-%m-%d")
+totdata$FECHA_INGRESO = as.Date(totdata$FECHA_INGRESO, format="%Y-%m-%d")
+totdata$FECHA_SINTOMAS = as.Date(totdata$FECHA_SINTOMAS, format="%Y-%m-%d")
 ndat <- totdata %>% group_by(FECHA_SINTOMAS, ENTIDAD_RES) %>% filter(RESULTADO == 1 & FECHA_SINTOMAS >= Dates[1] & FECHA_SINTOMAS <= Dates[2]) %>% summarise("I" = n()) %>% select(FECHA_SINTOMAS, ENTIDAD_RES, I)
 ndat <- ndat %>% filter(ENTIDAD_RES <= 32)
-
 days <- seq(as.Date(Dates[1]), as.Date(Dates[2]), 1)
+
+file.entidades$entidad <- as.character(file.entidades$entidad)
+
 for (enti in seq(length(unique(ndat$ENTIDAD_RES)))){
   days0 <- days[!(days %in% (ndat$FECHA_SINTOMAS[ndat$ENTIDAD_RES == file.entidades[enti, 1]]))]
-  tmp.ndat <- data.frame(FECHA_SINTOMAS = days0, ENTIDAD_RES = file.entidades[enti, 1], I = as.integer(0))
+  tmp.ndat <- data.frame(FECHA_SINTOMAS = as.Date(days0), ENTIDAD_RES = file.entidades[enti, 1], I = as.integer(0))
   tmp.ndat <- as.data.frame(tmp.ndat)
   ndat <- rbind(as.data.frame(ndat), tmp.ndat)
+  rm(tmp.ndat, days0)
 }
 
 for (enti in seq(length(unique(ndat$ENTIDAD_RES)))){
@@ -61,7 +63,6 @@ for (enti in seq(length(unique(ndat$ENTIDAD_RES)))){
 ndat <- as.data.frame(rbind(ndat, data.frame(ndat %>% group_by(FECHA_SINTOMAS) %>% summarise("I" = sum(I)), ENTIDAD_RES = "Estados Unidos Mexicanos")))
 names(ndat) <- c("Date", "State", "I")
 
-mob %>% select(State) %>% unique()
 ###############################################################
 mobData <- merge(ndat, mob, by = c("Date", "State"))
 ###############################################################
@@ -77,8 +78,7 @@ mobAMX$region <- c("Estados Unidos Mexicanos", "Ciudad de Mexico", "Aguascalient
 
 names(mobAMX)[2:length(names(mobAMX))] <- sub("X", "", names(mobAMX)[2:length(names(mobAMX))])
 mobDriv <- data.frame()
-for (state in seq(nrow(mobAMX)))
-{
+for (state in seq(nrow(mobAMX))){
   tmp.mobA <- data.frame(State = mobAMX$region[state], Date = names(mobAMX)[2:length(names(mobAMX))], Driving = as.numeric(mobAMX[state, 2:length(mobAMX)]))
   tmp.mobA$Driving[tmp.mobA$Date %in% c("2020.05.11", "2020.05.12")] <- mean(tmp.mobA$Driving[tmp.mobA$Date %in% c("2020.05.09", "2020.05.10", "2020.05.13", "2020.05.14")])
   mobDriv <- rbind(mobDriv, tmp.mobA)
@@ -86,11 +86,16 @@ for (state in seq(nrow(mobAMX)))
 }
 mobDriv$Date <- as.Date(mobDriv$Date, format="%Y.%m.%d")
 mobDriv$Driving <- mobDriv$Driving - 100
+
 ###############################################################
 mobData <- merge(mobData, mobDriv, by = c("Date", "State"))
 ###############################################################
 #mobData <- na.approx(mobData, fromLast = TRUE) ##solución posible (aunque ya corregí y no debe de haber NA's)
 mobData[is.na(mobData)] <- 0 #no muy profesional (ni útil)
+
+################################################################################
+########### NOW All DATA IS IN ONE DATAFRAME (mobData) #########################
+################################################################################
 
 ## Select State ##
 mobData %>% select("State") %>% unique() %>% as.character() ##all states list
@@ -160,7 +165,6 @@ for(i in seq(length(PoV.states$entidad))){                                      
 
   }
 }
-pca.vectors.rand <- pca.vectors
 
 
 ###################### Una vez creado el dataframe procedemos a obtener los angulos
@@ -178,49 +182,6 @@ z.val <- angle.matrix
 ########### Create randomized angles for performing Z-value test ##########
 pca.nvectors <- pca.vectors[, 2:length(pca.vectors)]
 
-############## First approach ##################################################
-random.vectors <- matrix(0, nrow=nrow(pca.nvectors), ncol=ncol(pca.nvectors))
-
-##create all possible [i,j] combinations
-combs <- c()
-for (i in seq(ncol(pca.nvectors)))
-  for (j in seq(nrow(pca.nvectors)))
-    combs <- append(combs, list(c(i, j)))
-
-##randomize samples##
-randomTimes <- 2 ##times to random list
-for (i in seq(randomTimes))
-  combs <- sample(combs)
-
-##assign new position in "random.vectors" to "pca.nvectors" variables
-count <- 1
-for (i in seq(nrow(pca.nvectors))){
-  for (j in seq(ncol(pca.nvectors))){
-    newj <- combs[[count]][[1]]
-    newi <- combs[[count]][[2]]
-    random.vectors[newi, newj] = as.numeric(pca.nvectors[i, j])
-    pca.nvectors[i, j]
-    count = count + 1
-  }
-}
-
-##evaluate angles between random variables created
-random.angles = c() ##all random angles
-norm_vec <- function(x) sqrt(sum(x^2))
-## Fill matrix with
-for(i in seq(nrow(random.vectors)-1)){
-  for(j in seq((i+1), nrow(random.vectors))){
-    temp.P <- random.vectors[i, ]
-    temp.Q <- random.vectors[j, ]
-    dot.product <- sum(temp.P * temp.Q)
-    angle.inrad <- acos(dot.product / (norm_vec(temp.P) * norm_vec(temp.Q)))
-    angle.indeg <- (angle.inrad * 180) / pi ##maybe it is not necessary to do this
-    random.matrix[i, j] <- angle.indeg ##just for "human" visualization
-    random.angles <- append(random.angles, angle.indeg) ##all random angles list
-  }
-}
-
-################ Second approach ###############################################
 random.angles = c()
 pca.nvectors2 <- sample(pca.nvectors)
 for(i in seq(nrow(pca.nvectors2)-1)){
